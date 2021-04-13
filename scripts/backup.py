0c625ca3ed499988106c19d58b91c1ea3ea1054e
jasmaa <jasonmaa3955@gmail.com>
2021-04-13 09:14:41 -0400

    Update backup script to only save new dated boards

--- scripts/backup.py
+++ scripts/backup.py
@@ -1,4 +1,5 @@
 import os
+import re
 import json
 import requests
 from dotenv import load_dotenv
@@ -6,6 +7,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 base_url = 'https://api.cloudflare.com/client/v4'
+
+def download_and_save(d, fname):
+    """Download and save board given key
+    """
+    res = requests.get(
+        f'{base_url}/accounts/{os.getenv("ACCOUNT_ID")}/storage/kv/namespaces/{os.getenv("BOARDS_ID")}/values/{d}',
+        headers={'Authorization': f'Bearer {os.getenv("API_TOKEN")}'},
+    )
+    with open(fname, 'w') as f:
+        json.dump(res.json(), f)
+        print(f'Saved {fname}')
 
 
 def backup():
@@ -17,20 +29,17 @@ def backup():
     )
 
     board_dates = [entry['name'] for entry in res.json()['result']]
+    board_dates = [d for d in board_dates if re.search(r'^\d{4}-\d{2}-\d{2}$', d)]
     fname = 'dates.json'
     with open(fname, 'w') as f:
         json.dump(board_dates, f)
         print(f'Saved {fname}')
 
+    # Save all new dated boards
     for d in board_dates:
-        res = requests.get(
-            f'{base_url}/accounts/{os.getenv("ACCOUNT_ID")}/storage/kv/namespaces/{os.getenv("BOARDS_ID")}/values/{d}',
-            headers={'Authorization': f'Bearer {os.getenv("API_TOKEN")}'},
-        )
-        fname = f'{d}.json'
-        with open(fname, 'w') as f:
-            json.dump(res.json(), f)
-            print(f'Saved {fname}')
+        fname = f'./data/{d}.json'
+        if not os.path.isfile(fname):
+            download_and_save(d, fname)
 
 
 if __name__ == '__main__':
